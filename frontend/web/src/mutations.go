52cb5c3792f155461427bcc534f3f62f7fb3a4c9
yjamesjbviele <tema-k572999642@rambler.ru>
2025-12-05 05:57:05 +0800

    add mutations.go, report-factory.py, dist.java

---
+++ frontend/web/src/mutations.go
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\mutations.go
+// Generated at: 2025-10-20T11:42:21.021Z
---
+++ report-factory.py
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\report-factory.py
+// Generated at: 2025-10-20T11:42:21.015Z
